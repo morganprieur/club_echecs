ae9a82332be9bd7903d6fe53cc61b685117eef7f
morgan_p <morgan.prieur@tetradis.com>
2023-02-13 08:14:48 +0100

    BUG : Abstract_model get_registered

--- models/abstract_model.py
+++ models/abstract_model.py
@@ -32,14 +32,22 @@ class AbstractModel(ABC):
     # Si le fichier JSON n'est pas vide : 
     # @staticmethod 
     def get_registered(self): 
-        # print(f'self AM35 : {self}') 
+        print(f'self AM35 : {self}') 
         # with open(f'tables/{self.table}.json', 'r') as file:  # ==> self.table inconnu 
         with open(f'tables/{self}.json', 'r') as file: 
             registered = json.load(file) 
         # print(f'registered AC16 : {registered}') 
         # print(f'type(registered) AC17 : {type(registered)}') 
         return registered 
-    
+### project.py 
+#     File "C:\Users\mprieur\Dropbox\Formation_OCR\P4\work\models\abstract_model.py", line 36, in get_registered
+#         with open(f'tables/{self.table}.json', 'r') as file:  # ==> self.table inconnu
+#     AttributeError: 'str' object has no attribute 'table'. Did you mean: 'title'? 
+
+### player_model.py 
+# File "C:\Users\mprieur\Dropbox\Formation_OCR\P4\work\models\abstract_model.py", line 37, in get_registered
+#     with open(f'tables/{self}.json', 'r') as file:
+# OSError: [Errno 22] Invalid argument: 'tables/<__main__.Player_model object at 0x000002EB64A4FC70>.json' 
 
     # def to_dict(self, exclude=None):
     #     exclude = exclude or []
@@ -60,9 +68,9 @@ class AbstractModel(ABC):
     #     pass 
 
     def serialize(self): 
-        """
+        """ 
             Abstract method for serialize the objects from the models. 
-        """
+        """ 
         # print(f'one_tournament TM172 : {self}') 
         if not self.check_if_json_empty(): 
             objects = self.get_registered() 
--- models/player_model.py
+++ models/player_model.py
@@ -1,7 +1,7 @@
 
-from .abstract_model import AbstractModel 
+# from .abstract_model import AbstractModel  
 # for tests : 
-# from abstract_model import AbstractModel 
+from abstract_model import AbstractModel 
 
 import json 
 
@@ -9,32 +9,34 @@ import json
 # class Player(Persist_entity): 
 class Player_model(AbstractModel): 
 
-    def __init__(self, lastname:str, firstname:str):  # , age, genre, rank, global_score 
+    def __init__(self, lastname:str, firstname:str, rank: int):  # , age, genre, rank, global_score 
         super().__init__('p_table') 
         self.lastname = lastname  
         self.firstname = firstname 
+        self.rank = rank 
         # self.age = age 
         # self.genre = genre 
         # self.rank = rank 
         # self.global_score = global_score 
         # players.append(self)   # pas bonnes pratiques 
 
-    def __str__(self): 
-        # born = '' 
-        # if self.genre == 'M\n' or self.genre == 'M': 
-        #     born = 'né'
-        # elif self.genre == 'F\n' or self.genre == 'F': 
-        #     born = 'née' 
-        # elif self.genre == 'A\n' or self.genre == 'A': 
-        #     born = 'né.e' 
-        # return f'{self.firstname} {self.lastname} {born} on {self.age} range: {self.rank}.'  # , global score: {self.global_score}.' 
-        return f'{self.firstname} {self.lastname}.'  # , global score: {self.global_score}.' 
+    # def __str__(self): 
+    #     # born = '' 
+    #     # if self.genre == 'M\n' or self.genre == 'M': 
+    #     #     born = 'né'
+    #     # elif self.genre == 'F\n' or self.genre == 'F': 
+    #     #     born = 'née' 
+    #     # elif self.genre == 'A\n' or self.genre == 'A': 
+    #     #     born = 'né.e' 
+    #     # return f'{self.firstname} {self.lastname} {born} on {self.age} range: {self.rank}.'  # , global score: {self.global_score}.' 
+    #     return f'{self.firstname} {self.lastname} classement : {self.rank}.'  # , global score: {self.global_score}.' 
 
 
     def to_dict(self): 
         return { 
             'lastname': self.lastname, 
-            'firstname': self.firstname 
+            'firstname': self.firstname, 
+            'rank': self.rank 
         } 
 
 
@@ -70,11 +72,12 @@ if __name__ == "__main__":
     # import site 
     # site.addsitedir('abstract_model') 
     new_player = {
-        "lastname": "Nom 130", 
-        "firstname": "Prénom 130" 
+        'lastname': 'Nom 130', 
+        'firstname': 'Prénom 130', 
+        'rank': 30 
     } 
     one_player = Player_model(**new_player) 
-    print(f'new_player PM77 : {new_player}') 
+    print(f'new_player PM80 : {new_player}') 
     # print(f'type(new_player) PM66 : {type(new_player)}') 
     one_player.serialize() 
 
